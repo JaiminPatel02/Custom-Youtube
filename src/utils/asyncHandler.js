3d83224c4a0712cbfc0d44f135a5c57c3eee9ce9
jaimin patel <132324275+JaiminPatel02@users.noreply.github.com>
2025-04-12 18:47:56 +0530

    old async

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,11 @@
 const asyncHandler = (requestHandler) => {
     return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch(next);
-    };
-};
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
 
-export { asyncHandler };
+
+export { asyncHandler }
 
 
 // productiion ma hoy avu to 
